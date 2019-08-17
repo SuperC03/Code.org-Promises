5648cf90af7eec10bc542d65105229cd69436199
SuperC03 <42807913+SuperC03@users.noreply.github.com>
2019-08-18 01:55:27 +0200

    Added More Comments and Descriptions

--- Promise.js
+++ Promise.js
@@ -1,43 +1,43 @@
-function Promise (_executable) {
+function Promise (_executable /* Gives User Access to 'resolve()' and 'reject()' */) {
   
-  var thenables = [];
-  var catchables = [];
-  var finalables = [];
-  var catchablesComplete = false, thenablesComplete = false;
-  var thisPromise = this;
+  var thenables = [];   // All 'then' Callback Functions
+  var catchables = [];  // All 'catch' Callback Functions
+  var finalables = [];  // All 'final' Callback Functions
+  var catchablesComplete = false, thenablesComplete = false; // If All 'catch' and 'then' Have Been Executed
+  var thisPromise = this;   // To Allow Chaining of Functions by Returning This Instance
   
-  _executable(function(response) {
-    thenables.forEach(function(thenable) {
-      thenable(response);
+  _executable(function(response) { // The 'resolve()' Handler
+    thenables.forEach(function(thenable) {  // Cycles Through All 'Thenables'
+      thenable(response); // Executes the 'Thenable'
     });
-    thenablesComplete = true;
-    if(thenablesComplete && catchablesComplete) {
-      finalables.forEach(function(finalable) {
-        finalable();
+    thenablesComplete = true; // Indicates All 'Thenables' Have Been Executed
+    if(thenablesComplete && catchablesComplete) { // Checks if All 'Thenables' and 'Catchables' Have Been Executed
+      finalables.forEach(function(finalable) {  // Cycles Through All 'Finalables'
+        finalable(); // Executes the 'Finalable'
       });
     }
-  }, function(error) {
-    catchables.forEach(function(catchable) {
-      catchable(error);
+  }, function(error) { // The 'reject()' Handler
+    catchables.forEach(function(catchable) {  // Cycles Through All 'Catchables'
+      catchable(error); // Executes the 'Catchable'
     });
-    catchablesComplete = true;
-    if(thenablesComplete && catchablesComplete) {
-      finalables.forEach(function(finalable) {
-        finalable();
+    catchablesComplete = true; // Indicates All 'Catchables' Have Been Executed
+    if(thenablesComplete && catchablesComplete) { // Checks if All 'Thenables' and 'Catchables' Have Been Executed
+      finalables.forEach(function(finalable) {  // Cycles Through All 'Finalables'
+        finalable(); // Executes the 'Finalable'
       });
     }
   });
   
-  this.then = function(_thenable) {
-    thenables.push(_thenable);
-    return thisPromise;
+  this.then = function(_thenable) { // Accepts a Function to be Called on 'resolve()'
+    thenables.push(_thenable); // Add to the List of 'Thenables'
+    return thisPromise; // Return Instance for Chaining
   };
-  this.catch = function(_catchable) {
-    catchables.push(_catchable);
-    return thisPromise;
+  this.catch = function(_catchable) { // Accepts a Function to be Called on 'catch()'
+    catchables.push(_catchable);  // Add to the List of 'Catchables'
+    return thisPromise; // Return Instance for Chaining
   };
-  this.finally = function(_finalable) {
-    finalables.push(_finalable);
-    return thisPromise;
+  this.finally = function(_finalable) { // Accepts a Function to be Called When all Others are Complete
+    finalables.push(_finalable);  // Add to the List of 'Finalables'
+    return thisPromise; // Return Instance for Chaining
   };
 }
