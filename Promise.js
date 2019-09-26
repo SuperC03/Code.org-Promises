62cce988312d20cac86e55a8a7d3349c75637255
Hampton Moore <me@hampton.pw>
2019-09-26 22:21:34 +0200

    Refactor how ables are run

--- Promise.js
+++ Promise.js
@@ -8,35 +8,41 @@ function Promise(_executable /* Gives User Access to 'resolve()' and 'reject()' 
     thenablesComplete = false; // If All 'catch' and 'then' Have Been Executed
   var thisPromise = this; // To Allow Chaining of Functions by Returning This Instance
 
-  this._runCatchable = function (error) {
+  this._runCatchable = function (response) {
+    // This is to protect from both resolve and reject being run
     if (this.state == "resolved") {
       return;
     }
     this.state = "reject";
-    catchables.forEach(function (catchable) { // Cycles Through All 'Catchables'
-      catchable(error); // Executes the 'Catchable'
-    });
-    catchablesComplete = true; // Indicates All 'Catchables' Have Been Executed
-    if (catchablesComplete) { // Checks if All 'Thenables' and 'Catchables' Have Been Executed
-      finalables.forEach(function (finalable) { // Cycles Through All 'Finalables'
-        finalable(); // Executes the 'Finalable'
-      });
+    if (catchables.length >= 1) {
+      response = catchables[0](response);
+      catchables.shift();
+      thisPromise._runCatchable(response);
+    } else {
+      thisPromise._runFinalables(response);
     }
   }
 
   this._runThenables = function (response) {
+    // This is to protect from both resolve and reject being run
     if (this.state == "reject") {
       return;
     }
     this.state = "resolved";
-    thenables.forEach(function (thenable) { // Cycles Through All 'Thenables'
-      thenable(response); // Executes the 'Thenable'
-    });
-    thenablesComplete = true; // Indicates All 'Thenables' Have Been Executed
-    if (thenablesComplete) { // Checks if All 'Thenables' and 'Catchables' Have Been Executed
-      finalables.forEach(function (finalable) { // Cycles Through All 'Finalables'
-        finalable(); // Executes the 'Finalable'
-      });
+    if (thenables.length >= 1) {
+      response = thenables[0](response);
+      thenables.shift();
+      thisPromise._runThenables(response);
+    } else {
+      thisPromise._runFinalables(response);
+    }
+  }
+
+  this._runFinalables = function (response) {
+    if (finalables.length >= 1) {
+      response = finalables[0](response);
+      finalables.shift();
+      thisPromise._runFinalables(response);
     }
   }
 
